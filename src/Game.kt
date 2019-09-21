0d8e2df229e756e2306370384a86cb646f9684d1
arellanog <arellanog@cent.gov.ar>
2019-09-21 13:43:32 -0300

    Listing 3.7 add conditionals expressions

--- src/Game.kt
+++ src/Game.kt
@@ -5,25 +5,28 @@ fun main() {
     val isImmortal = false
 
     //Aura
-    if (isBlessed && healthPoints > 50 || isImmortal){
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    if (auraVisible) {
         println("GREEN")
     } else {
         println("NONE")
     }
 
-    if (healthPoints == 100){
-        println(name + " is in excellent condition!")
+    val healthStatus = if (healthPoints == 100) {
+        " is in excellent condition!"
     } else if (healthPoints >= 90) {
-        println(name + " has a few scratches.")
+        " has a few scratches."
     } else if (healthPoints >= 75) {
-        if (isBlessed){
-            println(name + " has some minor wounds but is healing quite quickly!")
+        if (isBlessed) {
+            " has some minor wounds but is healing quite quickly!"
         } else {
-            println(name + " has some minor wounds.")
+            " has some minor wounds."
         }
     } else if (healthPoints >= 15) {
-        println(name + " looks pretty hurt.")
+        " looks pretty hurt."
     } else {
-        println(name + " is in awful condition!")
+        " is in awful condition!"
     }
+
+    println(name + healthStatus)
 }
