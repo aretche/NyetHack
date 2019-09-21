7a97e68a9380d25ce94cb477a6adf12b3c3586c0
arellanog <arellanog@cent.gov.ar>
2019-09-21 13:54:23 -0300

    Listing 3.11 using string template

--- src/Game.kt
+++ src/Game.kt
@@ -13,16 +13,16 @@ fun main() {
     }
 
     val healthStatus = when (healthPoints) {
-        100 -> " is in excellent condition!"
-        in 90..99 -> " has a few scratches."
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches."
         in 75..89 -> if (isBlessed) {
-            " has some minor wounds but is healing quite quickly!"
+            "has some minor wounds but is healing quite quickly!"
         } else {
-            " has some minor wounds."
+            "has some minor wounds."
         }
-        in 15..74 -> " looks pretty hurt."
-        else -> " is in awful condition!"
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
     }
 
-    println(name + healthStatus)
+    println("$name $healthStatus")
 }
